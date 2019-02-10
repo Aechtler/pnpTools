7361117f1bc2666ef60888d580f3e54cd4dee669
Mark Aechtler <aechtler@gmail.com>
2019-02-10 15:09:59 +0100

    feat(0): Add new dice animation

--- src/components/Dice.vue
+++ src/components/Dice.vue
@@ -1,16 +1,16 @@
 <template>
     <div class="dice">
-        <button type="button" class="btn btn-success / dice__dice" :class="{'dice__dice--d6': usedDice === 'd6'}" @click="dice('d6')"><font-awesome-icon icon="dice-d6" /></button>
-        <button type="button" class="btn btn-success / dice__dice" :class="{'dice__dice--d20': usedDice === 'd20'}" @click="dice('d20')"><font-awesome-icon icon="dice-d20" /></button>
-        <!--
-        <font-awesome-icon icon="dice-one" v-if="dice6 === 1" />
-        <font-awesome-icon icon="dice-two" v-if="dice6 === 2" />
-        <font-awesome-icon icon="dice-three" v-if="dice6 === 3" />
-        <font-awesome-icon icon="dice-four" v-if="dice6 === 4" />
-        <font-awesome-icon icon="dice-five" v-if="dice6 === 5" />
-        <font-awesome-icon icon="dice-six" v-if="dice6 === 6" />
-        -->
-        <input class="form-control / dice__result" type="number" v-model="dicedNumber" aria-describedby="basic-addon1" disabled />
+        <button type="button" class="btn btn-success / dice__dice" :class="{'dice__dice--d6': usedDice === 6}" @click="dice(6)"><font-awesome-icon icon="dice-d6" /></button>
+        <button type="button" class="btn btn-success / dice__dice" :class="{'dice__dice--d20': usedDice === 20}" @click="dice(20)"><font-awesome-icon icon="dice-d20" /></button>
+        <button type="button" class="btn btn-secondary / dice__dice dice__dice--result" :class="{'dice__dice--done': diceDone, 'dice__dice--dicing': !diceDone,}" disabled>
+            <font-awesome-icon icon="dice-one" v-if="hasDiced6Number(1)" />
+            <font-awesome-icon icon="dice-two" v-if="hasDiced6Number(2)" />
+            <font-awesome-icon icon="dice-three" v-if="hasDiced6Number(3)" />
+            <font-awesome-icon icon="dice-four" v-if="hasDiced6Number(4)" />
+            <font-awesome-icon icon="dice-five" v-if="hasDiced6Number(5)" />
+            <font-awesome-icon icon="dice-six" v-if="hasDiced6Number(6)" />
+            <span v-if="usedDice === 20">{{dicedNumber}}</span>
+        </button>
     </div>
 </template>
 
@@ -19,15 +19,32 @@
         data: function() {
             return {
                 usedDice: null,
-                dice6: null,
-                dice20: null,
-                dicedNumber: null
+                dicedNumber: null,
+                diceDone: false,
             };
         },
         methods: {
             dice (dice) {
+                var self = this;
+                var i = 0;
+
+                this.diceDone = false;
                 this.usedDice = dice;
-                this.dicedNumber = dice === 'd20' ? _.random(1, 20) : _.random(1, 6);
+
+                while (i < 10) {
+                    setTimeout(function () {
+                        self.dicedNumber = _.random(1, dice);
+                    }, 100*i );
+
+                    i++;
+                }
+
+                setTimeout(function () {
+                    self.diceDone = true;
+                }, 1000 );
+            },
+            hasDiced6Number(number) {
+                return this.usedDice === 6 && this.dicedNumber === number;
             }
         }
     };
